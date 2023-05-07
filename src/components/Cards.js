7eab880e3b23a2e9455cb6922d0555f733511bf1
Faizan <faizansuhail786@gmail.com>
2023-05-07 16:37:39 +0500

    Added Cards items and Created pages for menu links

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,19 +6,45 @@ import './Cards.css';
 function Cards() {
   return (
     <div className='cards'>
-        <h1>Check Out the Destination Images</h1>
-        <div className="crads__container">
-            <div className="cards__wrapper">
-                <div className="cards__items">
-                <Cardsitems 
-                src="images/img-9.jpg"
-                text="Explore the hidden waterfall deep inside the Amazon Jungle."
-                lable="Advanture"
-                path="/services"
-                />  
-                </div>
-            </div>
+      <h1>Check Out the Destination Images</h1>
+      <div className="crads__container">
+        <div className="cards__wrapper">
+          <ul className='cards__items'>
+            <Cardsitems
+              src='/public/images/img-9.jpg'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/services'
+            />
+            <Cardsitems
+              src='images/img-2.jpg'
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <Cardsitems
+              src='images/img-3.jpg'
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <Cardsitems
+              src='images/img-4.jpg'
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <Cardsitems
+              src='images/img-8.jpg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
         </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+export default function Contact (){
+    return <h1 className="contact">Contact</h1>        
+}
